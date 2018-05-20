ff52ea8938dd4e602cc1159971a30f55266d8abd
Logan Barnett <logustus@gmail.com>
2018-05-19 20:46:13 -0400

    use flow workaround to provide types plus a distributable version - also upgrade packages

---
+++ src/index.js
@@ -0,0 +1,15 @@
+// @flow
+
+export { default as chunk } from './chunk.js'
+export { default as vector } from './vector.js'
+export { default as mesh } from './mesh.js'
+export { default as grid } from './grid.js'
+export { default as voxel } from './voxel.js'
+export { default as voxelType } from './voxelType.js'
+export { default as default } from './cubed.js'
+
+export type { Vector3, Vector2 } from './vector.js'
+export type { Voxel } from './voxel.js'
+export type { Grid, GridCell } from './grid.js'
+export type { Chunk } from './chunk.js'
+export type { Side, Triangle } from './mesh.js'
